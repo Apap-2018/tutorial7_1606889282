59bdc9aa0eedd5105433d8e64135641c728133a4
Sarah Yunita <sarahyunita111@gmail.com>
2018-10-31 22:44:41 +0700

    yeaye

--- src/main/java/com/apap/tutorial7/rest/Setting.java
+++ src/main/java/com/apap/tutorial7/rest/Setting.java
@@ -2,4 +2,5 @@ package com.apap.tutorial7.rest;
 
 public class Setting {
 	final public static String dealerUrl = "https://4cf65630-d36e-4d8c-81ee-e8beeb3e7528.mock.pstmn.io";
+	final public static String factoryUrl = "https://www.carqueryapi.com/api/0.3";
 }
